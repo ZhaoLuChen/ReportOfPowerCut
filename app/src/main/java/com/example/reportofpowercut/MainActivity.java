351fb4407bf21fe81a11c8c473ac0c920ea5fe79
ZLC <737288893@qq.com>
2022-11-02 00:29:57 +0800

    2022.11.2晚0:27
    将班组选择从单选按钮改为了下拉菜单，现在可以根据县公司来变化班组选项，生成停电报备信息后，点击复制可直接跳转到微信

--- app/src/main/java/com/example/reportofpowercut/MainActivity.java
+++ app/src/main/java/com/example/reportofpowercut/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.reportofpowercut;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -36,6 +38,9 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import tool.PoiUtil;
 import tool.TaiQuModel;
@@ -44,7 +49,7 @@ import tool.TaiQuadapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String line,switchOfLine,classes="营配一班",county="印王";//所选线路及开关
+    private String line,switchOfLine,county = "印王公司",classes = "线路开关台区统计表（营配一班）.xls";//所选线路及开关
     private String[] strs,lines,switchs;//台区数组、线路数组、开关数组
     private int sum,num;//低压户数、台区数
     public ListView taiQuList;//台区列表
@@ -101,10 +106,10 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spinnerOfLine = findViewById(R.id.line);/*线路选择菜单*/
         Spinner spinnerOfswtich = findViewById(R.id.switch_of_line);/*开关选择开关*/
+        Spinner spinnerOfClasses = findViewById(R.id.classes);
         Button report_button = findViewById(R.id.report_bt);/*信息生成按键，点击后生成停电信息到粘贴板*/
         Button taiquButton = findViewById(R.id.taiqu);/*台区选择按钮，点击后跳转TaiQuActivity*/
         EditText timeText = findViewById(R.id.time_cut);/*输入停电时间*/
-        RadioGroup classRadgroup = (RadioGroup) findViewById(R.id.radioGroup);//班组选择
         RadioGroup countyRadgroup = (RadioGroup) findViewById(R.id.county_radioGroup);//区县公司选择
 
         //将台区信息统计表拷贝到手机下载文件夹
@@ -113,60 +118,56 @@ public class MainActivity extends AppCompatActivity {
         copyAssetsToDst(MainActivity.this, "excel/yaoxian", "/storage/emulated/0/Download/yaoxian/");
         copyAssetsToDst(MainActivity.this, "excel/yijun", "/storage/emulated/0/Download/yijun/");
 
-        final LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-        dialogView = inflater.inflate(R.layout.dialog, null,false);
-        ListView taiquList = (ListView)dialogView.findViewById(R.id.taiqu_list);
-        builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setView(dialogView);
+        initAdapter(spinnerOfClasses,new File(filepath_county).list());
 
         countyRadgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton radioButton = (RadioButton) findViewById(checkedId);
-
-                if (radioButton.getText().toString().equals("印王")){
+                county = "";
+                county =radioButton.getText().toString();
+                if (county.equals("印王")){
                     filepath_county = "/storage/emulated/0/Download/yinwang/";
                 }
-                if (radioButton.getText().toString().equals("宜君")){
+                if (county.equals("宜君")){
                     filepath_county ="/storage/emulated/0/Download/yijun/";
                 }
-                if (radioButton.getText().toString().equals("耀县")){
+                if (county.equals("耀县")){
                     filepath_county ="/storage/emulated/0/Download/yaoxian/";
                 }
-                if (radioButton.getText().toString().equals("新区")){
+                if (county.equals("新区")){
                     filepath_county ="/storage/emulated/0/Download/xinqu/";
                 }
-                county = radioButton.getText().toString()+"公司";
-                Toast.makeText(MainActivity.this,"切换到"+county,Toast.LENGTH_SHORT).show();
+                county = county+"公司";
+
+                filepath_class = Objects.requireNonNull(new File(filepath_county).list())[0];
+
+                File file = new File(filepath_county);
+                if (file.exists())
+                System.out.println(Arrays.toString(file.list()));
+
+                initAdapter(spinnerOfClasses,file.list());
             }
         });
 
-        classRadgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        spinnerOfClasses.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                RadioButton radioButton = (RadioButton) findViewById(i);
-                if (radioButton.getText().toString().equals("营配一班")){
-                    filepath_class = "线路开关台区统计表（供电服务班）.xls";
-                    classes = "营配一班";
-                }
-                if (radioButton.getText().toString().equals("营配二班")){
-                    filepath_class = "线路开关台区统计表（营配二班）.xls";
-                    classes = "营配二班";
-                }
-                if (radioButton.getText().toString().equals("营配四班")){
-                    filepath_class = "线路开关台区统计表（营配四班）.xls";
-                    classes = "营配三班";
-                }
-                classes = radioButton.getText().toString();
-                Toast.makeText(MainActivity.this,county+classes,Toast.LENGTH_SHORT).show();
-                excelFile = findFile();//每当点击按钮改变，就重新读取台区文件
-                initAdapter(spinnerOfLine);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                filepath_class  = spinnerOfClasses.getSelectedItem().toString();
+                classes = "";
+                classes = filepath_class.substring(filepath_class.indexOf("（") + 1, filepath_class.indexOf("）"));
+                System.out.println(classes);
+
+                excelFile = findFile(filepath_county,filepath_class);
+                lines = PoiUtil.getLinesFromExcel(excelFile);
+                initAdapter(spinnerOfLine,lines);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
-
-        //读取台区数据表格，获取表格中的线路
-        excelFile = findFile();
-        initAdapter(spinnerOfLine);
 
         /*线路菜单点击监听器，根据所选线路确定开关*/
         spinnerOfLine.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -174,8 +175,9 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 //获取被选择的线路
                 line = spinnerOfLine.getSelectedItem().toString();
+                System.out.println(line);
                 //根据被选择的线路，读取当前线路下的开关
-                switchs = PoiUtil.getSwitchsFormExcel(excelFile,spinnerOfLine.getSelectedItem().toString());
+                switchs = PoiUtil.getSwitchsFormExcel(excelFile,line);
                 //设置开关菜单数组适配器
                 ArrayAdapter<String> adapterOfSwitch = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_item,switchs);
                 adapterOfSwitch.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -184,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-
             }
         });
 
@@ -204,40 +205,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*台区选择按键监听器，根据所选线路和开关选择台区，点击后跳转TaiQuActivity*/
-        taiquButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*生成对话框，显示要取消的台区*/
-                List<TaiQuModel> taiQuModelList = new ArrayList<>();
-                String[] newTaiQuArray;//台区数组
-                int[] nums;//低压户数数组
-                taiQuModelList = PoiUtil.getTaiQuFromExcel(excelFile,line,switchOfLine);
-                newTaiQuArray = new String[taiQuModelList.size()];
-                nums = new int[taiQuModelList.size()];
-                num = taiQuModelList.size();
-                for (int j = 0;j<taiQuModelList.size();j++){
-                    newTaiQuArray[j] = taiQuModelList.get(j).getTaiqu();
-                    nums[j] = taiQuModelList.get(j).getNum();
-                    System.out.println(newTaiQuArray[j]+"低压户数"+nums[j]);
-                }
-
-                /*生成数组适配器，作为listview和台区数据的桥梁*/
-                TaiQuadapter adapter = new TaiQuadapter(MainActivity.this, R.layout.taiqu_item, taiQuModelList);
-                taiquList.setAdapter(adapter);
-                alert = builder.setTitle("以下台区将收到故障影响：")
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                            }
-                        })
-                        .create();
-                alert.show();
-            }
-        });
-
-
         //信息生成按键监听器
         report_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -249,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!TextUtils.isEmpty(timeText.getText())){
                         time = Integer.parseInt(timeText.getText().toString());//获取停电时间
                     }
-
+                    report =new StringBuffer("") ;
                     List<TaiQuModel> taiQuModelList = PoiUtil.getTaiQuFromExcel(excelFile,line,switchOfLine);
                     strs = new String[taiQuModelList.size()];
                     int[] nums = new int[taiQuModelList.size()];
@@ -279,6 +246,7 @@ public class MainActivity extends AppCompatActivity {
                                     ClipData mClipData = ClipData.newPlainText("lable", report);
                                     cm.setPrimaryClip(mClipData);
 
+                                    getWechatApi();
                                     /*将report的信息清空*/
                                     report =new StringBuffer("") ;
                                 }
@@ -330,7 +298,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private File findFile(){
+    private File findFile(String filepath_county,String filepath_class){
         File excelFile = new File(filepath_county+filepath_class);
         if(excelFile.exists()){
             System.out.println("找到文件"+excelFile.toString());
@@ -341,11 +309,36 @@ public class MainActivity extends AppCompatActivity {
         return  excelFile;
     }
 
-    private void initAdapter(Spinner spinnerOfLine){
+    private void initAdapter(Spinner spinner,String[] strs){
         //设置线路菜单数组适配器
-        ArrayAdapter<String> adapterOfLine = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item,lines);
+        ArrayAdapter<String> adapterOfLine = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item,strs);
         adapterOfLine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //为线路菜单设置适配器
-        spinnerOfLine.setAdapter(adapterOfLine);
+        spinner.setAdapter(adapterOfLine);
     }
+
+    public List<String> extractMessageByRegular(String msg){
+        List<String> list=new ArrayList<String>();
+        Pattern p = Pattern.compile("(\\((^\\))*\\))");
+        Matcher m = p.matcher(msg);
+        while(m.find()){
+            list.add(m.group().substring(1, m.group().length()-1));
+        }
+        return list;
+    }
+
+    private void getWechatApi(){
+        try {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            ComponentName cmp = new ComponentName("com.tencent.mm","com.tencent.mm.ui.LauncherUI");
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setComponent(cmp);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // TODO: handle exception
+        }
+    }
+
+
 }
